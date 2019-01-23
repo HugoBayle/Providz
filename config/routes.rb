b23f29cbb86b56fff064dddc668469c4a1ea2378
HugoBayle <h.bayle@hotmail.fr>
2019-01-23 11:59:52 +0100

    WIP home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'realisation', to: 'pages#realisation', as: :realisation
+  get 'cms', to: 'pages#cms', as: :cms
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'home', to: 'pages#home', as: :home
+  # get 'pages/realisations'
+  # get 'pages/cms'
+  # get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
